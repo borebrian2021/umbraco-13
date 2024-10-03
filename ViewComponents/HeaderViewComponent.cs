6d00c631f24248acfb9186c425d6bc2631f3c22f
Brian Koskei <bkimutai2021@gmail.com>
2024-10-03 23:00:44 +0300

    Added lots of files

---
+++ ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Freelancer.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
